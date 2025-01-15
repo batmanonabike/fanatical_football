9df6e2cad3da155bbc930af4ffe1089bdbfbaa7b
supastishn <omareeto2012@hotmail.com>
2025-01-15 23:58:32 +0300

    add deceleration, better code

--- ball/ball.gd
+++ ball/ball.gd
@@ -6,6 +6,9 @@ var dribbling: bool = false
 var player_dribbling: CharacterBody2D
 
 var bodies_colliding: Array[Node2D]
+
+var shooting: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.ball = self
@@ -15,21 +18,31 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	bodies_colliding = get_overlapping_bodies()
-	if bodies_colliding.size() == 1:
+	if bodies_colliding.size() == 1 and not shooting:
 		dribbling = true
 		player_dribbling = bodies_colliding[0]
-	if dribbling:
-		global_position = player_dribbling.get_node("Node2D/DribbleLocation").global_position
-
+	if !dribbling:
+		return
+	global_position = player_dribbling.get_node("Node2D/DribbleLocation").global_position
+	if Input.is_action_pressed("shoot"):
+		dribbling = false
+		print("teehee")
+		_shoot(10, player_dribbling.rotation)
+						
 # minimum y = 1.0
 # 1 = stadium
 # 10 = camera
 func simulate_y_level(y: float):
 	scale = original_scale * y
 
-	
-	
-	
+
+var target_position: Vector2
+func _shoot(strength: float, rotation: float):
+	shooting = true
+	target_position = global_position + (Vector2(strength, 0).rotated(rotation) * 100)
+	print(target_position)
+	global_position.move_toward(target_position, strength * 10)
+	shooting = false
 	
 	
 	
--- player/player.gd
+++ player/player.gd
@@ -2,28 +2,23 @@ extends CharacterBody2D
 
 
 const SPEED: float = 300.0
-
-var direction_x: float
-var direction_y: float
-
+const DECELERATION: float = 1000.0
+var direction: Vector2
 @onready var dribble_and_sprite = $Node2D
 func _ready():
 	add_collision_exception_with(Global.ball)
 
 func _physics_process(delta):
-	direction_x = Input.get_axis("left", "right")
-	direction_y = Input.get_axis("forward", "backward")
-	if direction_x:
-		velocity.x = direction_x * SPEED
+	direction = Input.get_vector("left", "right", "forward", "backward")
+	if direction.x:
+		velocity.x = direction.x * SPEED
 	else:
-		velocity.x = 0
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-	if direction_y:
-		velocity.y = direction_y * SPEED
+		velocity.x = move_toward(velocity.x, 0, DECELERATION * delta)
+	if direction.y:
+		velocity.y = direction.y * SPEED
 	else:
-		velocity.y = 0
-		#velocity.y = move_toward(velocity.y, 0, SPEED)
-	velocity = velocity.normalized() * SPEED
+		velocity.y = move_toward(velocity.y, 0, DECELERATION * delta)
+		
 	if velocity.x and not velocity.y:
 		dribble_and_sprite.rotation = deg_to_rad(270) if velocity.x > 0 else deg_to_rad(90)
 	elif velocity.y and not velocity.x:
@@ -35,8 +30,7 @@ func _physics_process(delta):
 			dribble_and_sprite.rotation = deg_to_rad(90 + 35) if velocity.x < 0 else deg_to_rad(245)
 			
 	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
-	
-	
+
 	
 	
 	
