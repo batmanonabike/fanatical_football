1ed16cd0811a1b197db4e29820b65b07fa359e19
supastishn <omareeto2012@hotmail.com>
2025-01-15 23:29:21 +0300

    Dribbling added

--- ball/ball.gd
+++ ball/ball.gd
@@ -1,6 +1,11 @@
 extends Area2D
 
+var original_scale: Vector2 = scale
 
+var dribbling: bool = false
+var player_dribbling: CharacterBody2D
+
+var bodies_colliding: Array[Node2D]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.ball = self
@@ -8,5 +13,32 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	bodies_colliding = get_overlapping_bodies()
+	if bodies_colliding.size() == 1:
+		dribbling = true
+		player_dribbling = bodies_colliding[0]
+	if dribbling:
+		global_position = player_dribbling.get_node("Node2D/DribbleLocation").global_position
+
+# minimum y = 1.0
+# 1 = stadium
+# 10 = camera
+func simulate_y_level(y: float):
+	scale = original_scale * y
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- player/player.gd
+++ player/player.gd
@@ -3,13 +3,16 @@ extends CharacterBody2D
 
 const SPEED: float = 300.0
 
+var direction_x: float
+var direction_y: float
+
+@onready var dribble_and_sprite = $Node2D
 func _ready():
 	add_collision_exception_with(Global.ball)
 
 func _physics_process(delta):
-
-	var direction_x: float = Input.get_axis("left", "right")
-	var direction_y: float = Input.get_axis("forward", "backward")
+	direction_x = Input.get_axis("left", "right")
+	direction_y = Input.get_axis("forward", "backward")
 	if direction_x:
 		velocity.x = direction_x * SPEED
 	else:
@@ -21,6 +24,35 @@ func _physics_process(delta):
 		velocity.y = 0
 		#velocity.y = move_toward(velocity.y, 0, SPEED)
 	velocity = velocity.normalized() * SPEED
+	if velocity.x and not velocity.y:
+		dribble_and_sprite.rotation = deg_to_rad(270) if velocity.x > 0 else deg_to_rad(90)
+	elif velocity.y and not velocity.x:
+		dribble_and_sprite.rotation = deg_to_rad(0) if velocity.y > 0 else deg_to_rad(180)
+	elif velocity.y and velocity.x:
+		if velocity.y > 0:
+			dribble_and_sprite.rotation = deg_to_rad(45) if velocity.x < 0 else deg_to_rad(-45)
+		else:
+			dribble_and_sprite.rotation = deg_to_rad(90 + 35) if velocity.x < 0 else deg_to_rad(245)
+			
 	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
 	
-	print(collision)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
